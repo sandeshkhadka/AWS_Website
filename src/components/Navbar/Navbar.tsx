61b7caecedd9c9625357bcaddfdca9fbe5a0fbcb
Sandip Gyawali <sandipgyawali10@gmail.com>
2025-08-22 20:38:08 +0545

    feat(add): shadcn component block addition

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import React from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
